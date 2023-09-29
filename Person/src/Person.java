7674852bc0f0a7704c17d8d4cda342968a8c5bd7
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-09-29 17:21:47 +0200

    Add files via upload

---
+++ Person/src/Person.java
@@ -0,0 +1,52 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        System.out.println(this.firstName);
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        System.out.println(this.lastName);
+        return this.lastName;
+    }
+
+    public int getAge() {
+        System.out.println("The Age is: " + this.age);
+        return this.age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public boolean isTeen() {
+        return this.age > 12 && this.age < 20;
+    }
+
+    public String getFullName() {
+        if (this.firstName.isEmpty()){
+            return this.lastName;
+        } else if (this.lastName.isEmpty()) {
+            return this.firstName;
+        } else {
+            return (this.firstName + " " + this.lastName);
+        }
+    }
+
+}
